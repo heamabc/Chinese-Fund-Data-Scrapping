ebdd144e44bdbc3bbaedc0f249c30db818439097
heamabc <32771919+heamabc@users.noreply.github.com>
2019-12-18 11:42:14 +0800

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# It is in UTC, 15 in UTC is 23 in Hong Kong as HKT is UTC + 8
+# To stop the program, press ctrl + c in the command
+while :
+    do
+        TIME=$(date '+%H');
+        if [ $TIME == '15' ];
+        then
+            echo Time matched, current hour is $TIME, run program..., ;
+            python service.py;
+            echo Sleep 1 hour;
+            sleep 3600;
+            
+        else
+            echo Time not matched, sleep 1 hour, current hour is $TIME;
+            sleep 3600;
+        fi
+    done
